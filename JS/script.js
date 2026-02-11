83aeba2fdd0d5b6e58f4bfc88526905f2cef1f69
Mominulislam1234423 <mominulislamshawon88@gmail.com>
2026-02-11 14:10:44 +0600

    Vocabularies section olmost complete

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,27 @@
-console.log('hello')
+
+const loadLessons = () => {
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+        .then(res => res.json())
+        .then(json => displayLessons(json.data))
+};
+
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/(${id})`
+    console.log(url)
+}
+
+const displayLessons = (lessons) => {
+    const lavelContinar = document.getElementById('lavel-continar');
+    lavelContinar.innerHTML = "";
+
+    for (let lesson of lessons) {
+        const btnDiv = document.createElement('div');
+        btnDiv.innerHTML = `
+    <button onclick="loadLevelWord(${lesson.level_no})"
+   class="border-2 border-blue-600 text-blue-600 px-2 py-1 rounded-md font-semibold transition duration-300 hover:bg-blue-500 hover:text-white">
+   <i class="fa-solid fa-question font-bold"></i><span class="font-bold">lesson - ${lesson.level_no}</span></button>
+    `
+    lavelContinar.append(btnDiv);
+    }
+}
+loadLessons()
